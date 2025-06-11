624c16247af0cab908861eaddb496c2c8643612b
lys <lyscalefi@gmail.com>
2025-06-11 15:07:32 -0300

    Ajusta organizacao de layout

---
+++ GithubSearchAPI/Home/Repository/ViewController/Cell/UserProfileCell.swift
@@ -0,0 +1,85 @@
+//
+//  UserProfileCell.swift
+//  GithubSearchAPI
+//
+//  Created by Amaryllis Baldrez on 11/06/25.
+//
+
+import UIKit
+
+final class UserProfileCell: UITableViewCell {
+
+    private lazy var avatarImageView: UIImageView = {
+        let image = UIImageView()
+        return avatarImageView
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    private lazy var ownerLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    private lazy var languageLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupLayout()
+    }
+
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+
+    func configure(with viewModel: GithubUserViewModel) {
+        titleLabel.text = viewModel.name
+        ownerLabel.text = "👤 \(viewModel.login)"
+        descriptionLabel.text = viewModel.description
+        languageLabel.text = "🧠 \(viewModel.language)"
+
+        avatarImageView.load(url: viewModel.avatarURL)
+    }
+
+    private func setupLayout() {
+        [avatarImageView, titleLabel, ownerLabel, descriptionLabel, languageLabel].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            contentView.addSubview($0)
+        }
+
+        avatarImageView.layer.cornerRadius = 25
+        avatarImageView.clipsToBounds = true
+        avatarImageView.contentMode = .scaleAspectFill
+
+        NSLayoutConstraint.activate([
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12),
+            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 12),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 50),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 50),
+
+            titleLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 12),
+            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 12),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -12),
+
+            ownerLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            ownerLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 4),
+
+            descriptionLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            descriptionLabel.topAnchor.constraint(equalTo: ownerLabel.bottomAnchor, constant: 4),
+            descriptionLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
+
+            languageLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            languageLabel.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 4),
+            languageLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -12)
+        ])
+    }
+}
